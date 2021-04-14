63b4f5c45b25a893b8a2fbb99b82e46211ea985f
Gulchaman Mammadova <gulchaman.mammadova@gmail.com>
2021-04-13 22:05:01 -0400

    completed product creation

--- dao/product_dao.php
+++ dao/product_dao.php
@@ -2,7 +2,7 @@
     require_once('abstract_dao.php');
     require_once('./model/product.php');
 
-    class EmployeeDAO extends AbstractDAO { 
+    class ProductDAO extends AbstractDAO {
         function __construct() {
             try{
                 parent::__construct();
@@ -11,7 +11,7 @@
             }
         }  
         
-        public function get_product($product_id){
+        public function get_product($product_id) {
             $query = 'SELECT * FROM products WHERE id = ?';
             $stmt = $this->mysqli->prepare($query);
             $stmt->bind_param('i', $product_id);
@@ -21,11 +21,30 @@
             $product = false;
             if($result->num_rows == 1){
                 $temp = $result->fetch_assoc();
-                $product = new Product($temp['id'], $temp['name'], $temp['address'], $temp['salary']);
+                $product = new Product($temp['id'], $temp['title'], $temp['description'], $temp['price'], $temp['is_featured']);
             }
             
             $result->free();
             return $product_id;
+        }
+
+        public function create_image($product_id, $name) {
+            $query = 'INSERT INTO images (name, product_id) VALUES (?, ?)';
+            $stmt = $this->mysqli->prepare($query);
+            $stmt->bind_param('si', $name, $product_id);
+            $stmt->execute();
+            $stmt->close();
+
+            return $this->mysqli->insert_id;
+        }
+        public function create_product($title, $description, $price, $is_featured) {
+            $query = 'INSERT INTO products (title, description, price, is_featured) VALUES (?, ?, ?, ?)';
+            $stmt = $this->mysqli->prepare($query);
+            $stmt->bind_param('ssii', $title, $description, $price, $is_featured);
+            $stmt->execute();
+            $stmt->close();
+
+            return $this->mysqli->insert_id;
         }
     }
 ?>
--- upload.php
+++ upload.php
@@ -1,18 +1,37 @@
 <?php
-  if (isset($_POST["submit"])) {
-    $file_name = $_FILES['image_file']['name'];
-    $file_tmp_name = $_FILES['image_file']['tmp_name'];
-    $upload_path = getcwd() . "/images/products" . basename($file_name);
-    $is_uploaded = move_uploaded_file($file_tmp_name, $upload_path);
-    if ($is_uploaded) {
-      echo "1";
-    } else{
-      echo "0";
-    }
+    require_once('./dao/product_dao.php');
 
-    // TODO: create product.
-    error_log("xiyar: " . $_POST["product_title"]);
-  }
+    if (isset($_POST["submit"])) {
+        $product_dao = new ProductDAO();
+
+        // Create the product
+        $is_featured = 0;
+        if ($_POST["product_is_featured"] == 'on') {
+            $is_featured = 1;
+        }
+        $product_id = $product_dao->create_product(
+            $_POST["product_title"],
+            $_POST["product_description"],
+            $_POST["product_price"],
+            $is_featured
+        );
+
+        // Store product image and register it in the database
+        // and associate with the product.
+        $product_image_file = $_FILES['product_image_file'];
+        $file_name = $product_image_file['name'];
+        $file_extension = pathinfo($file_name, PATHINFO_EXTENSION);
+        $file_tmp_name = $product_image_file['tmp_name'];
+        $image_id = $product_dao->create_image($product_id, $file_name);
+        $upload_path = getcwd() . "/images/products/" . $image_id . '.' . $file_extension;
+        $is_uploaded = move_uploaded_file($file_tmp_name, $upload_path);
+
+        if (isset($product_id) && isset($is_uploaded)) {
+            header('Location: upload.php?success=1');
+        } else {
+            header('Location: upload.php?success=0');
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -61,11 +80,38 @@
         </nav>
     </header>
     <main>
+        <div style="text-align: center;">
+            <?php
+                if (isset($_GET['success'])) {
+                    if ($_GET['success'] == '1') {
+                        echo '<p>Successfully created.</p>';
+                    } else {
+                        echo '<p>Error creating the product.</p>';
+                    }
+                }
+            ?>
         </div>
         <section>
             <form action="upload.php" method="POST" enctype="multipart/form-data">
-                <input type="text" placeholder="Product title" name="product_title" />
-                <input type="file" accept="image/*" name="image_file">
+                <label for="product_title">Product title</label>
+                <input type="text" placeholder="Product title" name="product_title" style="width: 415px" />
+
+                <label for="product_description">Product description</label>
+                <textarea name="product_description" rows="8" cols="50"></textarea>
+
+                <label for="product_price">Product price</label>
+                <input type="number" name="product_price"/>
+
+                <div>
+                    <input type="checkbox" name="product_is_featured"/>
+                    <label for="product_is_featured">Product is featured</label>
+                </div>
+
+                <div>
+                    <label for="product_image_file">Product image</label>
+                    <input type="file" accept="image/*" name="product_image_file">
+                </div>
+
                 <button type="submit" id="btn-search" name="submit">Create</button>
             </form>
         </section>
